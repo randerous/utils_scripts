6500db9625f4a8da65aef0c6f88c7c224baafe5a
randerous <1521498542@qq.com>
2024-03-29 16:26:03 +0800

    add bcache operation

---
+++ util/ceph/bcache.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cur=`dirname $0`
+source $cur/../common/highlight.sh
+function turn_off()
+{
+cd /sys/block
+for i in `ls /sys/block/ | grep bcache`
+do
+	echo writeback > $i/bcache/cache_mode
+	echo 80 > $i/bcache/writeback_percent
+	echo 0 > $i/bcache/writeback_running
+	echo 0 > $i/bcache/cache/congested_write_threshold_us
+	echo 0 > $i/bcache/cache/congested_read_threshold_us
+	echo 0 > $i/bcache/sequential_cutoff
+	#cd $i;sh /root/util/common/ls.sh;cd ..
+done
+}
+
+function turn_on()
+{
+cd /sys/block
+for i in `ls /sys/block/ | grep bcache`
+do
+	echo 1 > $i/bcache/writeback_running
+done
+}
+
+if [[ $# == 0 ]]
+then
+	
+	highlight turn_off
+	turn_off
+else
+	highlight turn_on
+	turn_on
+fi
