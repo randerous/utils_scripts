6f902c9fd7aa1b2b11a505219ff9079c7abfe824
randerous <1521498542@qq.com>
2024-04-12 16:53:22 +0800

    add s5 auto_test

--- util/s5/config.sh
+++ util/s5/config.sh
@@ -5,3 +5,15 @@ blks=`lsblk | grep $size | awk '{print $1}'`
 runtime=600
 oldtime=`cat $cur/tmp | grep elapse | awk -F 'elapsed=' '{print $2}' | awk -F ',' '{print $1}'`
 sed -i "s/elapsed=$oldtime/elapsed=$runtime/g" $cur/tmp
+
+for i in $blks
+do
+	lsblk | grep ${i}1 &>/dev/null
+
+	if [[ $? != 0 ]] 
+	then
+		echo $i 1
+		parted -s /dev/$i mklabel gpt; 
+		parted -s /dev/$i mkpart xfs 0% 4T
+	fi
+done
