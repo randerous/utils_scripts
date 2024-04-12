2d3ced5dd529e0c925df1c3104e7056175fb747e
randerous <1521498542@qq.com>
2024-04-12 23:19:06 +0800

    add collect_result for s5

--- util/s5/collect_test.sh
+++ util/s5/collect_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in `ls`; do echo -n "$i "; cat $i |  grep avg | awk '{print $2}'; done | sort -rhk 2
+for i in `ls`; do str=`smartctl -a /dev/$i | grep -i serial | awk '{print $3}'`; echo -n "$i $str "; cat $i |  grep avg | awk '{print $2}'; done | sort -rhk 3
